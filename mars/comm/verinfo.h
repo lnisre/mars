d48c03dc9a0a6bcd5d0d20b95b2a9bb00a7fc4ef
hesslergao <hesslergao@tencent.com>
2017-06-23 16:01:43 +0800

    fix心跳的buff2rsp

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "053db13"
-#define MARS_PATH "HEAD"
+#define MARS_REVISION "4f9c1d3"
+#define MARS_PATH "master"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2017-02-15 09:51:04"
-#define MARS_TAG "default"
+#define MARS_BUILD_TIME "2017-06-23 15:57:36"
+#define MARS_TAG "trunk_[2017-06-23_15.57]@git-4f9c1d3@hesslergao"
 
 #endif
